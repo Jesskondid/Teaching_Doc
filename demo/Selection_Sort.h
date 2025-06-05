70e60ec8b7e3359f164e4c3341dddfe021860325
EDY <EDY@JESS-LAPTOP>
2025-06-05 14:25:01 +0800

    new_sample添加项目文件。

---
+++ demo/Selection_Sort.h
@@ -0,0 +1,15 @@
+// —°‘Ò≈≈–Ú
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
