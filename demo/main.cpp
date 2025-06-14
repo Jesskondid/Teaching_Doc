e445c3e9e240a1d7532ebe70cad84febefbadbce
Jess <2476698206@qq.com>
2025-06-14 11:31:23 +0800

    添加排序功能并输出结果
    在 `main.cpp` 中添加了两个数组 `arr1` 和 `arr2`，并初始化为相同的值。调用了 `bubbleSort` 和 `selectionSort` 函数对这两个数组进行排序，并输出了经过冒泡排序后的数组 `arr1` 的内容。

--- demo/main.cpp
+++ demo/main.cpp
@@ -13,7 +13,15 @@
 #include "Binary_Insertion_Sort.h"
 using namespace std;
 int main() {
-    
+	const int n = 10;
+	int arr1[n] = { 64, 34, 25, 12, 22, 11, 90, 45, 78, 56 };
+	int arr2[n] = { 64, 34, 25, 12, 22, 11, 90, 45, 78, 56 };
+	bubbleSort(arr1, n);
+	selectionSort(arr2, n);
+	cout << "Bubble Sort: ";
+	for (int i = 0; i < n; ++i) {
+		cout << arr1[i] << " ";
+	}
 
     return 0;
 }
